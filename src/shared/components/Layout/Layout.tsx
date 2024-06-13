05eb8f6b1d44034e9b8e22791d8c622c7bd284ab
norrico31 <norricobiason31@gmail.com>
2024-06-13 18:06:00 +0800

    react pro sidebar

--- src/shared/components/Layout/Layout.tsx
+++ src/shared/components/Layout/Layout.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
 import Topbar from './Topbar'
+import Sidebar from './Sidebar'
 
 export default function Layout() {
     return (
         <div className='app'>
+            <Sidebar />
             <main className="content">
                 <Topbar />
-
+                {/* <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/team" element={<Team />} />
+                    <Route path="/contacts" element={<Contacts />} />
+                    <Route path="/invoices" element={<Invoices />} />
+                    <Route path="/form" element={<Form />} />
+                    <Route path="/calendar" element={<Calendar />} />
+                    <Route path="/bar" element={<Bar />} />
+                    <Route path="/pie" element={<Pie />} />
+                    <Route path="/line" element={<Line />} />
+                    <Route path="/faq" element={<FAQ />} />
+                    <Route path="/geography" element={<Geography />} />
+                </Routes> */}
             </main>
         </div>
     )
