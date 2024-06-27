67f5be5cdd09f872ecdbb274d7821e7a8ddffc16
norrico31 <norricobiason31@gmail.com>
2024-06-28 07:55:00 +0800

    hero icons

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
-// import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
 import { projectsDao } from '../shared/dao/ProjectDao';
 import { Table, Pagination } from '../shared/components';
@@ -50,22 +50,22 @@ export default function Dashboard() {
 
 
 function Modal({ open, onClose }: { open: boolean; onClose: () => void }) {
-    return <Dialog className="relative z-10" open={open} onClose={() => false}>
+    return <Dialog className="relative z-10" open={open} onClose={onClose}>
         <DialogBackdrop
             transition
             className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
         />
 
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-4">
+        <div className="fixed inset-0 z-10 w-screen ">
+            <div className="flex min-h-full  items-end justify-center p-4 text-center sm:items-center sm:p-4">
                 <DialogPanel
                     transition
                     className="relative transform overflow-x-auto rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-2xl lg:px-3 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
                 >
-                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div className="bg-white overflow-y-auto px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                         <div className="sm:flex sm:items-start">
                             <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                                {/* <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" /> */}
+                                <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
                             </div>
                             <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                                 <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
--- src/shared/components/Layout/Topbar.tsx
+++ src/shared/components/Layout/Topbar.tsx
@@ -1,5 +1,5 @@
-import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md'
 import { useThemeCtx } from '../../contexts/DarkMode'
+import { SunIcon, MoonIcon } from '@heroicons/react/24/outline'
 
 export default function Topbar({ onClick }: { onClick: () => void }) {
     const { theme, toggleTheme } = useThemeCtx()
@@ -11,12 +11,12 @@ export default function Topbar({ onClick }: { onClick: () => void }) {
                     <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                 </svg>
             </button>
-            <button onClick={toggleTheme} className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <button onClick={toggleTheme} className="inline-flex transition items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                 <span className="sr-only">Toggle dark mode</span>
                 {theme ?
-                    <MdOutlineLightMode size={25} />
+                    <SunIcon className="h-6 w-6 text-white-500" aria-hidden="true" />
                     :
-                    <MdOutlineDarkMode size={25} />
+                    <MoonIcon className="h-6 w-6 text-white-500" aria-hidden="true" />
                 }
             </button>
         </header>
